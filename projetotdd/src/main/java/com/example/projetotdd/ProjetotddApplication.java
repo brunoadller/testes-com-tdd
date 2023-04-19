5477ff8478b57d79d5920dce4d7e4206b8ec2618
brunoadller <adller676@gmail.com>
2023-04-19 15:53:26 -0300

    send tdd test

---
+++ projetotdd/src/main/java/com/example/projetotdd/ProjetotddApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetotdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotddApplication.class, args);
+	}
+
+}
---
+++ projetotdd/src/test/java/com/example/projetotdd/ProjetotddApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.projetotdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotddApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
+/*
+ * Criar uma api de vendas
+ * venda igual ou acima de 1000, 00 = 15%
+ * vendas abaixo de 1000 = 10% 
+ * Classe Comissao que tenha um metodo calcular que saiba devolver o valor da comissao
+ */
